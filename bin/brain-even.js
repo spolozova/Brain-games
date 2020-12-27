083363cc7b47887ecbd7985de6381f0e6c7d2467
Svetlana Polozova <svetabjs@gmail.com>
2020-12-27 21:01:53 +0300

    renamed game for brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import getRandomNumber, { rightAnswer, rule } from '../scr/games.js';
+import getRandomNumber, { rightAnswer, rule } from '../scr/gameEven.js';
 import theGame from '../scr/common-functions.js';
 
 console.log('Welcome to the Brain Games!');
