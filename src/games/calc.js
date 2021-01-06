090d0ae0577fe216fcf30b491bdd960289ac9785
Svetlana Polozova <svetabjs@gmail.com>
2021-01-06 16:52:55 +0300

    fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,12 +2,7 @@ import _ from 'lodash';
 import randomNumber from '../utils.js';
 
 export const RULE = 'What is the result of the expression?';
-const calculator = () => {
-  const ranNum1 = randomNumber();
-  const ranNum2 = randomNumber();
-  const operator = _.sample(['+', '-', '*']);
-  return `${ranNum1} ${operator} ${ranNum2}`;
-};
+const calculator = () => `${randomNumber()} ${_.sample(['+', '-', '*'])} ${randomNumber()}`;
 export const rightAnswer = (str) => {
   const separator = ' ';
   let answer = 0;
