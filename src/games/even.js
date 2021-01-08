3a39058f725897805ab5bade97b416b7abfeadca
Svetlana Polozova <svetabjs@gmail.com>
2021-01-08 13:44:20 +0300

    add answer function

--- src/games/even.js
+++ src/games/even.js
@@ -4,9 +4,11 @@ import getRandomNumber from '../utils.js';
 const runGameEven = () => {
   const RULE = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+  const getRightAnswer = (number) => (number % 2 === 0 ? 'yes' : 'no');
+
   const getQuestionAndAnswer = () => {
     const question = getRandomNumber();
-    const answer = (question % 2 === 0 ? 'yes' : 'no');
+    const answer = getRightAnswer(question);
     return [question, answer];
   };
 
