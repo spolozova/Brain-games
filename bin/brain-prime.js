69981bac16522224282b001ded1f33d7dc671e64
Svetlana Polozova <svetabjs@gmail.com>
2021-01-08 13:44:56 +0300

    change game prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import runGames from '../src/index.js';
-import getRandomNumber from '../src/utils.js';
-import rightAnswer, { RULE } from '../src/games/prime.js';
+import runGamePrime from '../src/games/prime.js';
 
-console.log('Welcome to the Brain Games!');
-runGames(rightAnswer, getRandomNumber, RULE);
+runGamePrime();
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,12 +1,27 @@
-export const RULE = 'Answer "yes" if given number is prime. Otherwise answer "no"';
-export default (number) => {
-  if (number < 2) {
-    return 'no';
-  }
-  for (let i = 2; i <= number / 2; i += 1) {
-    if (number % i === 0) {
+import runGame from '../index.js';
+import getRundomNumber from '../utils.js';
+
+const runGamePrime = () => {
+  const RULE = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+  const getRightAnswer = (number) => {
+    if (number < 2) {
       return 'no';
     }
-  }
-  return 'yes';
+    for (let i = 2; i <= number / 2; i += 1) {
+      if (number % i === 0) {
+        return 'no';
+      }
+    }
+    return 'yes';
+  };
+
+  const getQuestionAndAnswer = () => {
+    const question = getRundomNumber();
+    const answer = getRightAnswer(question);
+    return [question, answer];
+  };
+
+  runGame(RULE, getQuestionAndAnswer);
 };
+export default runGamePrime;
