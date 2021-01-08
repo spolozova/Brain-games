eef296f26a2aeaf10c41b6e74180a652901392b8
Svetlana Polozova <svetabjs@gmail.com>
2021-01-08 12:57:13 +0300

    change game even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-import getRandomNumber from '../src/utils.js';
-import rightAnswer, { RULE } from '../src/games/even.js';
-import runGames from '../src/index.js';
+import runGameEven from '../src/games/even.js';
 
-console.log('Welcome to the Brain Games!');
-
-runGames(rightAnswer, getRandomNumber, RULE);
+runGameEven();
--- src/games/even.js
+++ src/games/even.js
@@ -1,2 +1,16 @@
-export default (number) => (number % 2 === 0 ? 'yes' : 'no');
-export const RULE = 'Answer "yes" if the number is even, otherwise answer "no".';
+import runGame from '../index.js';
+import getRandomNumber from '../utils.js';
+
+const runGameEven = () => {
+  const RULE = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+  const getQuestionAndAnswer = () => {
+    const question = getRandomNumber();
+    const answer = (question % 2 === 0 ? 'yes' : 'no');
+    return [question, answer];
+  };
+
+  runGame(RULE, getQuestionAndAnswer);
+};
+
+export default runGameEven;
